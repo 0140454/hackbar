e2e4cc6ffd02474317ffa0684061c605298a3488
0140454 <s1994928@yahoo.com.tw>
2022-06-26 12:51:58 +0800

    Move font styles to plugins/vuetify.ts

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,10 @@
 // Styles
+import '@fontsource/roboto/100.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/900.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
