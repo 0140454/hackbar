def0d781227bdac160f7c98db9b2c3fcb3bafa02
0140454 <s1994928@yahoo.com.tw>
2022-07-11 23:31:48 +0800

    Add patched curlconverter
    Since `web-tree-sitter` from NPM is not a ES module, vite cannot extract
    `tree-sitter.wasm` into an asset, we use `exec` protocol to build an
    all-in-one version for ourselves.
    
    Moreover, we also build `curlconverter` without top-level-await for
    ourselves because rollup cannot generate code in IIFE format if there is
    a top-level-await statement.
    And, we need vite to extract `tree-sitter-bash.wasm` into an asset.

---
+++ build/curlconverter.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+const child_process = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+const buildDir = execEnv.buildDir
+const packageJsonPath = path.join(buildDir, 'package.json')
+const projectDir = process.env.PROJECT_CWD
+
+// Get tarball URL
+const tarballUrl = child_process
+  .execFileSync('npm', ['view', 'curlconverter', 'dist.tarball'])
+  .toString()
+  .trim()
+
+// Download tarball
+const tarballPath = path.join(execEnv.tempDir, 'tarball.tgz')
+child_process.execFileSync('curl', ['-s', tarballUrl, '-o', tarballPath])
+
+// Extract tarball
+child_process.execFileSync(
+  'tar',
+  ['-xf', tarballPath, '--strip-components=1'],
+  { cwd: buildDir },
+)
+
+// Patch packages
+child_process.execFileSync(
+  'patch',
+  ['-p', '1', '-i', path.join(projectDir, 'build/curlconverter.patch')],
+  { cwd: buildDir },
+)
+const newPackageJson = child_process
+  .execFileSync('jq', [
+    'del(.bin) | del(.browser) | .resolutions."tree-sitter" = "link:node_modules/.blocked.package"',
+    packageJsonPath,
+  ])
+  .toString()
+  .trim()
+fs.writeFileSync(packageJsonPath, newPackageJson)
+fs.writeFileSync(
+  path.join(buildDir, 'tree-sitter-bash.wasm?url.ts'),
+  'export default ""',
+)
+child_process.execFileSync('rm', [
+  '-rf',
+  path.join(buildDir, 'dist/src'),
+  path.join(buildDir, 'tools'),
+  path.join(buildDir, 'src/bash-parser.ts'),
+  path.join(buildDir, 'src/cli.ts'),
+])
+
+// Prepare package
+child_process.execFileSync('yarn', ['set', 'version', 'berry'], {
+  cwd: buildDir,
+})
+child_process.execFileSync(
+  'yarn',
+  ['config', 'set', 'nodeLinker', 'node-modules'],
+  { cwd: buildDir },
+)
+child_process.execFileSync('yarn', ['install'], { cwd: buildDir })
+child_process.execFileSync('yarn', ['run', 'compile'], { cwd: buildDir })
+
+// Cleanup
+child_process.execFileSync('rm', ['-rf', path.join(buildDir, 'node_modules')])
+child_process.execFileSync('find', [
+  path.join(buildDir, 'dist'),
+  '-name',
+  'tree-sitter-bash.wasm?url.*',
+  '-exec',
+  'rm',
+  '-rf',
+  '{}',
+  ';',
+])
